3c772d453cf46c20d6bdfd4221f56276bd12ae3c
John McCloskey <31834702+gettodaze@users.noreply.github.com>
2025-06-10 09:08:30 +0900

    add player movements

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,4 +3,20 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+	[Export] private float speed = 5.0f;
+	private Vector2 direction = new();
+
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+		Velocity = new(direction.X, 0, direction.Y);
+		Velocity *= speed;
+		MoveAndSlide();
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
+		direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+	}
 }
