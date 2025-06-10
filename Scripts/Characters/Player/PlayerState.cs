f86282912a8ca93818324794f40e91cc5a1c4ef3
John McCloskey <31834702+gettodaze@users.noreply.github.com>
2025-06-10 12:14:52 +0900

    add state scripts to scripts folder

---
+++ Scripts/Characters/Player/PlayerState.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public abstract partial class PlayerState : Node
+{
+    protected Player characterNode;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        characterNode = GetOwner<Player>();
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+}
