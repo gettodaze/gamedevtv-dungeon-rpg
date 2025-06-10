ee1c546a4e4ffd170ab2472d997dcffea6897bed
John McCloskey <31834702+gettodaze@users.noreply.github.com>
2025-06-10 12:22:12 +0900

    get all states

---
+++ Scripts/Characters/Player/StateMachine.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+using System.Linq;
+
+public partial class StateMachine : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		PlayerState[] states = GetChildren().OfType<PlayerState>().ToArray();
+		GD.Print(states);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
