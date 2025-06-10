bb480d2c55a06b17abc06069d4b218e087a96269
John McCloskey <31834702+gettodaze@users.noreply.github.com>
2025-06-10 12:03:14 +0900

    add ready methods

--- Scenes/Characters/Player/PlayerIdleState.cs
+++ Scenes/Characters/Player/PlayerIdleState.cs
@@ -6,6 +6,8 @@ public partial class PlayerIdleState : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Player characterNode = GetOwner<Player>();
+		characterNode.AnimPlayerNode.Play(GameConstants.ANIM_IDLE);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scenes/Characters/Player/PlayerMoveState.cs
+++ Scenes/Characters/Player/PlayerMoveState.cs
@@ -6,6 +6,8 @@ public partial class PlayerMoveState : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Player characterNode = GetOwner<Player>();
+		characterNode.AnimPlayerNode.Play(GameConstants.ANIM_MOVE);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
