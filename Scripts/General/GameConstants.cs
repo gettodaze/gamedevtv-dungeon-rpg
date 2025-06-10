65526f90e8db8b73ca948f69cdd46051d1581893
John McCloskey <31834702+gettodaze@users.noreply.github.com>
2025-06-10 13:51:05 +0900

    Revert "wip will revert to recommended implementation"
    This reverts commit 3f7ec76c7e28544de24228f94ae1a9f4f93c5f22.

--- Scripts/General/GameConstants.cs
+++ Scripts/General/GameConstants.cs
@@ -3,18 +3,11 @@ public class GameConstants
     // Animations
     public const string ANIM_IDLE = "Idle";
     public const string ANIM_MOVE = "Move";
-    public const string ANIM_DASH = "Dash";
-
 
     // Inputs
     public const string INPUT_MOVE_LEFT = "MoveLeft";
     public const string INPUT_MOVE_RIGHT = "MoveRight";
     public const string INPUT_MOVE_FORWARD = "MoveForward";
     public const string INPUT_MOVE_BACKWARD = "MoveBackward";
-
-    // States
-    public const string PLAYER_STATE_IDLE = ANIM_IDLE;
-    public const string PLAYER_STATE_MOVE = ANIM_MOVE;
-    public const string PLAYER_STATE_DASH = ANIM_DASH;
 
 }
