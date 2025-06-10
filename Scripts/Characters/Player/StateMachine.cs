3f7ec76c7e28544de24228f94ae1a9f4f93c5f22
John McCloskey <31834702+gettodaze@users.noreply.github.com>
2025-06-10 13:46:46 +0900

    wip will revert to recommended implementation

--- Scripts/Characters/Player/StateMachine.cs
+++ Scripts/Characters/Player/StateMachine.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 public partial class StateMachine : Node
 {
@@ -8,7 +9,21 @@ public partial class StateMachine : Node
 	public override void _Ready()
 	{
 		PlayerState[] states = GetChildren().OfType<PlayerState>().ToArray();
+		var stateMap = states.ToDictionary(s => s.Name);
 		GD.Print(string.Join(", ", states.Select(s => s.Name)));
+		GD.Print(stateMap);
+		var expectedKeys = new HashSet<string> { "Idle", "Run", "Jump" };
+		var actualKeys = stateMap.Keys.ToHashSet();
+
+		if (!actualKeys.Equals(expectedKeys))
+		{
+			var unexpected = actualKeys.Except(expectedKeys);
+			var missing = expectedKeys.Except(actualKeys);
+
+			throw new InvalidOperationException(
+				$"Unexpected state keys.\nMissing: {string.Join(", ", missing)}\nUnexpected: {string.Join(", ", unexpected)}"
+			);
+		}
 
 	}
 
