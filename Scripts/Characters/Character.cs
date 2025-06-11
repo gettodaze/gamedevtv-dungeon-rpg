d9366933d2d9a75a9751c28fac4c63afcf151b1e
John McCloskey <31834702+gettodaze@users.noreply.github.com>
2025-06-12 08:39:01 +0900

    enemy inherits character

---
+++ Scripts/Characters/Character.cs
@@ -0,0 +1,50 @@
+using Godot;
+using System;
+
+public partial class Character : CharacterBody3D
+{
+    [Export(PropertyHint.Range, "0,20,1")] private float speed = 5.0f;
+    [Export(PropertyHint.Range, "0,50,1")] public float dashSpeed = 15.0f;
+    [Export(PropertyHint.Range, "0,10,0.01")] public float gravity = 0.2f;
+    [Export(PropertyHint.Range, "0,10,0.01")] public float jumpSpeed = 5f;
+    [ExportGroup("Required Nodes")]
+    [Export] public AnimationPlayer AnimPlayerNode { get; private set; }
+    [Export] public Sprite3D Sprite3DNode { get; private set; }
+    [Export] public StateMachine StateMachine { get; private set; }
+    public Vector2 direction = new();
+
+    public override void _Ready()
+    {
+    }
+
+    public void Jump()
+    {
+        GD.Print("Jump!");
+        Velocity += Vector3.Up * jumpSpeed;
+    }
+
+    public void Move(bool dash = false)
+    {
+        var scaledDir = direction * (dash ? dashSpeed : speed);
+        // float velY = IsOnFloor() ? 0 : Velocity.Y - 0.1f;
+        Velocity = new(scaledDir.X, Velocity.Y, scaledDir.Y);
+        if (!IsOnFloor()) Velocity += Vector3.Down * gravity;
+        Flip();
+        MoveAndSlide();
+    }
+
+    private void Flip()
+    {
+        if (direction.X < 0)
+        {
+            Sprite3DNode.FlipH = true;
+        }
+        else if (direction.X > 0)
+        {
+            Sprite3DNode.FlipH = false;
+        }
+    }
+
+
+
+}
--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -1,49 +1,8 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody3D
+public partial class Player : Character
 {
-	[Export(PropertyHint.Range, "0,20,1")] private float speed = 5.0f;
-	[Export(PropertyHint.Range, "0,50,1")] public float dashSpeed = 15.0f;
-	[Export(PropertyHint.Range, "0,10,0.01")] public float gravity = 0.2f;
-	[Export(PropertyHint.Range, "0,10,0.01")] public float jumpSpeed = 5f;
-	[ExportGroup("Required Nodes")]
-	[Export] public AnimationPlayer AnimPlayerNode { get; private set; }
-	[Export] public Sprite3D Sprite3DNode { get; private set; }
-	[Export] public StateMachine StateMachine { get; private set; }
-	public Vector2 direction = new();
-
-	public override void _Ready()
-	{
-	}
-
-	public void Jump()
-	{
-		GD.Print("Jump!");
-		Velocity += Vector3.Up * jumpSpeed;
-	}
-
-	public void Move(bool dash = false)
-	{
-		var scaledDir = direction * (dash ? dashSpeed : speed);
-		// float velY = IsOnFloor() ? 0 : Velocity.Y - 0.1f;
-		Velocity = new(scaledDir.X, Velocity.Y, scaledDir.Y);
-		if (!IsOnFloor()) Velocity += Vector3.Down * gravity;
-		Flip();
-		MoveAndSlide();
-	}
-
-	private void Flip()
-	{
-		if (direction.X < 0)
-		{
-			Sprite3DNode.FlipH = true;
-		}
-		else if (direction.X > 0)
-		{
-			Sprite3DNode.FlipH = false;
-		}
-	}
 
 	public void SetInputDirection()
 	{
@@ -51,5 +10,4 @@ public partial class Player : CharacterBody3D
 		direction = Input.GetVector(GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT, GameConstants.INPUT_MOVE_FORWARD, GameConstants.INPUT_MOVE_BACKWARD);
 		GD.Print($"Set character direction from {oldDir} to {direction}");
 	}
-
 }
