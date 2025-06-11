7f30737c6330bdf378f754bd873a3cbc2d34ee66
John McCloskey <31834702+gettodaze@users.noreply.github.com>
2025-06-11 12:10:18 +0900

    adjust how current state is set

--- Scripts/Characters/Player/StateMachine.cs
+++ Scripts/Characters/Player/StateMachine.cs
@@ -4,15 +4,27 @@ using System.Linq;
 
 public partial class StateMachine : Node
 {
-	[Export] private PlayerState currentState;
+	private bool isReady = false;
+	private PlayerState _currentState;
+	[Export]
+	private PlayerState CurrentState
+	{
+		get => _currentState;
+		set
+		{
+			GD.Print($"Setting state from {_currentState} to {value}");
+			_currentState = value;
+			if (isReady) value.EnableState();
+		}
+	}
 	private PlayerState[] states;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		states = GetChildren().OfType<PlayerState>().ToArray();
 		GD.Print(string.Join(", ", states.Select(s => s.Name)));
-		currentState.EnableState();
-
+		isReady = true;
+		CurrentState.EnableState();
 
 	}
 
