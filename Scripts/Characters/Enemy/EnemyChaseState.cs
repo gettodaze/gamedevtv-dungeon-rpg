d78cf1b58077aa8495d6d66c4582ada67a1540da
John McCloskey <31834702+gettodaze@users.noreply.github.com>
2025-06-12 20:39:54 +0900

    implement chase state

--- Scripts/Characters/Enemy/EnemyChaseState.cs
+++ Scripts/Characters/Enemy/EnemyChaseState.cs
@@ -1,10 +1,39 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class EnemyChaseState : CharacterState
 {
 	public override bool IsEligibleForRandom => false;
 	protected override string AnimationString => GameConstants.ANIM_MOVE;
+	private CharacterBody3D target;
+
+	public override void EnableState()
+	{
+		base.EnableState();
+		target = characterNode.Area3DNode.GetOverlappingBodies().First() as CharacterBody3D;
+		characterNode.Area3DNode.BodyExited += HandleBodyExit;
+	}
+
+	private void HandleBodyExit(Node3D body)
+	{
+		characterNode.StateMachine.SwitchState<EnemyIdleStateA>();
+	}
+
+
+	public override void DisableState()
+	{
+		base.DisableState();
+		characterNode.Area3DNode.BodyExited -= HandleBodyExit;
+
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+		characterNode.NavigationAgentNode.TargetPosition = target.GlobalPosition;
+		characterNode.MoveToDestination();
+	}
 
 
 }
