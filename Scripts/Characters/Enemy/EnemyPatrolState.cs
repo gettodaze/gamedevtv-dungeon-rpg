5a366c4058e5be16e32b94fc97794a125a5fd3b0
John McCloskey <31834702+gettodaze@users.noreply.github.com>
2025-06-12 19:01:29 +0900

    dynamically create timer instead

--- Scripts/Characters/Enemy/EnemyPatrolState.cs
+++ Scripts/Characters/Enemy/EnemyPatrolState.cs
@@ -5,31 +5,48 @@ public partial class EnemyPatrolState : CharacterState
 {
 	protected override string AnimationString => GameConstants.ANIM_MOVE;
 	private int pathIdx = 0;
+	private Timer idleTimer;
+	[Export(PropertyHint.Range, "0,20,0.1")] private float maxIdleTime = 4;
+	private bool processMove = true;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		idleTimer = new Timer();
+		idleTimer.Timeout += () => { processMove = true; characterNode.AnimPlayerNode.Play(GameConstants.ANIM_MOVE); };
+		AddChild(idleTimer);
+	}
 	public override void EnableState()
 	{
 		base.EnableState();
 		pathIdx = 0;
-		incrementPathIdx();
-		characterNode.NavigationAgentNode.NavigationFinished += incrementPathIdx;
+		handleReachedPathVertex();
+		characterNode.NavigationAgentNode.NavigationFinished += handleReachedPathVertex;
 	}
 
 	public override void DisableState()
 	{
 		base.DisableState();
-		characterNode.NavigationAgentNode.NavigationFinished -= incrementPathIdx;
+		characterNode.NavigationAgentNode.NavigationFinished -= handleReachedPathVertex;
 
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		base._PhysicsProcess(delta);
-		characterNode.MoveToDestination();
+		if (processMove) characterNode.MoveToDestination();
 
 	}
 
-	public void incrementPathIdx()
+	public void handleReachedPathVertex()
 	{
+		processMove = false;
+		characterNode.AnimPlayerNode.Play(GameConstants.ANIM_IDLE);
+		RandomNumberGenerator rng = new();
+		idleTimer.WaitTime = rng.RandfRange(0, maxIdleTime);
+		idleTimer.Start();
 		pathIdx = (pathIdx + 1) % characterNode.PathNode.Curve.PointCount;
 		characterNode.NavigationAgentNode.TargetPosition = characterNode.GetPathIdxGlobalPosition(pathIdx);
+
 	}
 }
