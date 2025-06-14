4c31121890b509f6c61d087837f50bd90e8b41ac
John McCloskey <31834702+gettodaze@users.noreply.github.com>
2025-06-14 19:25:27 +0900

    add start scene

---
+++ Scripts/UI/UIController.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public partial class UIController : Control
+{
+    private Dictionary<ContainerType, UIContainer> containers;
+
+    public override void _Ready()
+    {
+        containers = GetChildren().Where((element) => element is UIContainer).Cast<UIContainer>().ToDictionary(
+            (element) => element.container
+        );
+
+        containers[ContainerType.Start].Visible = true;
+    }
+}
